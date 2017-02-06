9820dd90f7f399cbf9b4ec43e9dc92eeb43c211b
Przemyslaw Sempruch <przemyslaws@kainos.com>
2017-02-06 15:54:51 +0000

    First code

---
+++ src/main/java/uk/gov/dvsa/motr/subscriptionloader/handler/EventHandler.java
@@ -0,0 +1,17 @@
+package uk.gov.dvsa.motr.subscriptionloader.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class EventHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventHandler.class);
+
+    public void handle(Object request, Context context) {
+
+        logger.info("Request: {}, context: {}", request, context);
+    }
+}
