9c152d2c270f697956597392013d6e1d1e0cfad4
Przemos <przemos@users.noreply.github.com>
2017-02-13 17:24:52 +0100

    Added remote call timings aspect (#27)

---
+++ webapp/src/main/java/uk/gov/dvsa/motr/web/performance/PerfAspect.java
@@ -0,0 +1,53 @@
+package uk.gov.dvsa.motr.web.performance;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+import uk.gov.dvsa.motr.eventlog.EventLogger;
+import uk.gov.dvsa.motr.eventlog.TimedEvent;
+
+@Aspect
+public class PerfAspect {
+
+    @Pointcut("within(uk.gov.dvsa.motr..*)")
+    public void withinMotrWeb() {
+    }
+
+    @Pointcut("execution(public * *(..))")
+    public void anyPublicMethod() {
+    }
+
+    @Pointcut("execution(* uk.gov.dvsa.motr.remote.vehicledetails.VehicleDetailsClient.*(..))")
+    public void vehicleDetailsClientCall() {
+    }
+
+    @Pointcut("execution(* uk.gov.dvsa.motr.web.component.subscription.persistence.DynamoDbSubscriptionRepository.*(..))")
+    public void repositoryCall() {
+    }
+
+    @Pointcut("vehicleDetailsClientCall() || repositoryCall()")
+    public void remoteCall() {
+    }
+
+    @Around("withinMotrWeb() && anyPublicMethod() && remoteCall()")
+    public Object around(ProceedingJoinPoint pointcut) throws Throwable {
+
+        Object response;
+        long start = System.currentTimeMillis();
+        String name = pointcut.getSignature().toShortString();
+
+        try {
+
+            response = pointcut.proceed();
+
+        } finally {
+
+            long end = System.currentTimeMillis() - start;
+            EventLogger.logEvent(new TimedEvent().setName(name).setTime(end));
+        }
+
+        return response;
+    }
+}
