ce69bb03f58c5e2c064d6b6266bcf5246ef0fc7e
Przemyslaw Sempruch <przemyslaws@kainos.com>
2017-03-01 13:19:09 +0000

    DynamoDbProducer using queries instead of scans fix

--- src/main/java/uk/gov/dvsa/motr/subscriptionloader/processing/producer/DynamoDbProducer.java
+++ src/main/java/uk/gov/dvsa/motr/subscriptionloader/processing/producer/DynamoDbProducer.java
@@ -40,30 +40,39 @@ public class DynamoDbProducer implements SubscriptionProducer {
 
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM-dd");
 
-        ValueMap valueMap = new ValueMap()
-                .withString(":due_date1", firstNotificationDate.format(dateFormatter))
-                .withString(":due_date2", secondNotificationDate.format(dateFormatter));
+        QuerySpec querySpec2WeeksAgo = new QuerySpec()
+                .withKeyConditionExpression("mot_due_date_md = :due_date")
+                .withValueMap(new ValueMap().withString(":due_date", firstNotificationDate.format(dateFormatter)));
 
-        QuerySpec querySpec = new QuerySpec()
-                .withKeyConditionExpression("mot_due_date_md = :due_date1 OR mot_due_date_md = :due_date2")
-                .withValueMap(valueMap);
+        QuerySpec querySpec1MonthAgo = new QuerySpec()
+                .withKeyConditionExpression("mot_due_date_md = :due_date")
+                .withValueMap(new ValueMap().withString(":due_date", secondNotificationDate.format(dateFormatter)));
 
-        ItemCollection<QueryOutcome> resultCollection = dueDateIndex.query(querySpec);
 
-        Iterator<Item> iterator = resultCollection.iterator();
+        ItemCollection<QueryOutcome> result2weeksAgo = dueDateIndex.query(querySpec2WeeksAgo);
+        ItemCollection<QueryOutcome> result1MonthAgo = dueDateIndex.query(querySpec1MonthAgo);
+
+        Iterator<Item> iterator2WeeksAgo = result2weeksAgo.iterator();
+        Iterator<Item> iterator1MonthAgo = result1MonthAgo.iterator();
 
         return new Iterator<Subscription>() {
 
             @Override
             public boolean hasNext() {
 
-                return iterator.hasNext();
+                return iterator2WeeksAgo.hasNext() || iterator1MonthAgo.hasNext();
             }
 
             @Override
             public Subscription next() {
 
-                Item item = iterator.next();
+                Item item;
+                if (iterator2WeeksAgo.hasNext()) {
+                    item = iterator2WeeksAgo.next();
+                } else {
+                    item = iterator1MonthAgo.next();
+                }
+
                 logger.debug("item is {}", item);
                 LocalDate motDueDate = LocalDate.parse(item.getString("mot_due_date"), DateTimeFormatter.ISO_DATE);
                 return new Subscription()
