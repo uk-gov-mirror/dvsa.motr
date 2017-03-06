f78f799ca87a71f8201bd30dabe62053dadb0f59
Przemos <przemos@users.noreply.github.com>
2017-03-06 21:49:54 +0000

    Fix in DynamoDbSubscriptionRepositoryTest - findById uses asynchronous index there sometimes operations done in the main table are not yet visible in the index and test has to wait for the result (#59)

--- webapp/src/integration-test/java/uk/gov/dvsa/motr/web/component/subscription/persistence/DynamoDbSubscriptionRepositoryTest.java
+++ webapp/src/integration-test/java/uk/gov/dvsa/motr/web/component/subscription/persistence/DynamoDbSubscriptionRepositoryTest.java
@@ -13,15 +13,23 @@ import uk.gov.dvsa.motr.test.integration.dynamodb.fixture.model.SubscriptionItem
 import uk.gov.dvsa.motr.test.integration.dynamodb.fixture.model.SubscriptionTable;
 import uk.gov.dvsa.motr.web.component.subscription.model.Subscription;
 
+import java.util.Optional;
+import java.util.function.Supplier;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import static uk.gov.dvsa.motr.test.integration.dynamodb.DynamoDbIntegrationHelper.client;
 import static uk.gov.dvsa.motr.test.integration.dynamodb.DynamoDbIntegrationHelper.region;
 import static uk.gov.dvsa.motr.test.integration.dynamodb.DynamoDbIntegrationHelper.subscriptionTableName;
 
+import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
+import static java.lang.Thread.sleep;
 import static java.time.format.DateTimeFormatter.ofPattern;
+import static java.util.Optional.empty;
 
 public class DynamoDbSubscriptionRepositoryTest {
 
@@ -42,7 +50,11 @@ public class DynamoDbSubscriptionRepositoryTest {
 
         fixture.table(new SubscriptionTable().item(expectedSubscription)).run();
 
-        Subscription actualSubscription = repo.findById(expectedSubscription.getId()).get();
+        Subscription actualSubscription = waitUntilPresent(
+                () -> repo.findById(expectedSubscription.getId()),
+                true,
+                5000
+        ).get();
 
         assertEquals(actualSubscription.getEmail(), expectedSubscription.getEmail());
         assertEquals(actualSubscription.getVrm(), expectedSubscription.getVrm());
@@ -61,7 +73,11 @@ public class DynamoDbSubscriptionRepositoryTest {
 
         repo.save(subscription);
 
-        Subscription actualSubscription = repo.findById(subscription.getId()).get();
+        Subscription actualSubscription = waitUntilPresent(
+                () -> repo.findById(subscription.getId()),
+                true,
+                5000
+        ).get();
 
         assertEquals(subscriptionItem.getEmail(), actualSubscription.getEmail());
         assertEquals(subscriptionItem.getVrm(), actualSubscription.getVrm());
@@ -132,6 +148,36 @@ public class DynamoDbSubscriptionRepositoryTest {
 
         repo.delete(subscription);
 
-        assertFalse(repo.findById(sub.getId()).isPresent());
+        waitUntilPresent(() -> repo.findById(sub.getId()), false, 5000);
+    }
+
+    /**
+     * Waits till a supplier that returns Optional returns it with a certain state
+     *
+     * @param optionalSupplier supplier that provides the result
+     * @param isPresent        asserted state of Optional
+     * @param timeout          maximum time that the asserted state will be waited for
+     */
+    private <T> Optional<T> waitUntilPresent(Supplier<Optional<T>> optionalSupplier, boolean isPresent, long timeout) {
+
+        long current = currentTimeMillis();
+        boolean conditionSatisfied = false;
+        Optional<T> optional = empty();
+
+        while (!conditionSatisfied || (currentTimeMillis() - current) < timeout) {
+            try {
+                optional = optionalSupplier.get();
+                conditionSatisfied = optional.isPresent() == isPresent;
+                if (conditionSatisfied) {
+                    break;
+                }
+                sleep(100);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+        assertTrue(format("Assert timed out after %s ms", timeout), conditionSatisfied);
+
+        return optional;
     }
 }
