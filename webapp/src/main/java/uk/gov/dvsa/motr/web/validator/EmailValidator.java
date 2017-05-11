91012a11d8d57d921df89e3e87302dc42e71ec54
Raffaele Cecco <raffaelececco@Raffaeles-MacBook-Pro.local>
2017-05-11 15:19:38 +0100

    Added validation to exactly match gov-notify. Added additional valid/invalid email addresses to test against. Removed unused dependency from gradle.build

--- webapp/src/main/java/uk/gov/dvsa/motr/web/validator/EmailValidator.java
+++ webapp/src/main/java/uk/gov/dvsa/motr/web/validator/EmailValidator.java
@@ -1,7 +1,7 @@
 package uk.gov.dvsa.motr.web.validator;
 
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailValidator {
 
@@ -23,9 +23,7 @@ public class EmailValidator {
             return false;
         }
 
-        try {
-            new InternetAddress(email, true).validate();
-        } catch (AddressException e) {
+        if (!isValidAccordingToGovNotify(email)) {
             this.message = EMAIL_INVALID_MESSAGE;
             return false;
         }
@@ -36,5 +34,54 @@ public class EmailValidator {
     public String getMessage() {
 
         return this.message;
+    }
+
+    /**
+     * Java version of the email validator found here:
+     * https://github.com/alphagov/notifications-utils/blob/master/notifications_utils/recipients.py
+     */
+    private boolean isValidAccordingToGovNotify(String email) {
+
+        Pattern emailRegex = Pattern.compile("^[^\\s\";@]+@([^.@][^@]+)$");
+        Pattern hostnamePart = Pattern.compile("^(xn-|[a-z0-9]+)(-[a-z0-9]+)*$");
+        Pattern tldPart = Pattern.compile("^([a-z]{2,63}|xn--([a-z0-9]+-)*[a-z0-9]+)$");
+
+        Matcher emailMatcher = emailRegex.matcher(email);
+        if (!emailMatcher.matches()) {
+            return false;
+        }
+
+        String hostname = emailMatcher.group(1);
+
+        if (hostname.contains("..")) {
+            return false;
+        }
+
+        try {
+            // Each part of the hostname must be < 64 chars.
+            hostname = java.net.IDN.toASCII(hostname);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+
+        String[] parts = hostname.split(Pattern.quote("."));
+        int partsLength = parts.length;
+
+        if (hostname.length() > 253 || partsLength < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < partsLength; i++) {
+            String part = parts[i];
+            if (part == null || part.isEmpty() || part.length() > 63 || !hostnamePart.matcher(part).matches()) {
+                return false;
+            }
+        }
+
+        if (!tldPart.matcher(parts[partsLength - 1]).matches()) {
+            return false;
+        }
+
+        return true;
     }
 }
