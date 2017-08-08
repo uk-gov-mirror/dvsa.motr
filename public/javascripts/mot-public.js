57a29cb64a911d603c49bf59164b2531da8b50f2
Joel Sudworth <j.sudworth2@kainos.com>
2017-08-08 07:36:23 +0100

    BL-5573 updating WFA for version number

--- public/javascripts/mot-public.js
+++ public/javascripts/mot-public.js
@@ -1,1 +1,69 @@
-!function(globals){"use strict";var DVSA={Modules:{},Utilities:{},Events:!1,init:function(events){var x;this.Events=events||!1;for(x in DVSA.Modules)DVSA.Modules.hasOwnProperty(x)&&"function"==typeof DVSA.Modules[x].init&&DVSA.Modules[x].init()}};globals.DVSA=DVSA}(this),function(globals){"use strict";var JSTest;globals.DVSA,JSTest=function(){},JSTest.prototype.init=function(){},globals.DVSATest={jsTest:JSTest}}(this),function(globals){"use strict";globals.DVSA.Modules.DVSATest=DVSATest,globals.DVSA.init($({}))}(this);
+/*jslint browser: true, evil: false, plusplus: true */
+/*global $ */
+
+(function (globals) {
+    'use strict';
+
+    var DVSA = {
+
+        Modules: {},
+
+        Utilities: {},
+
+        // Events: $({}), // Example of using JQuery for events.
+        Events: false,
+
+        init: function (events) {
+            var x;
+
+            this.Events = events || false;
+
+            for (x in DVSA.Modules) {
+                if (DVSA.Modules.hasOwnProperty(x) && typeof (DVSA.Modules[x].init) === "function") {
+                    DVSA.Modules[x].init();
+                }
+            }
+        }
+    };
+
+    globals.DVSA = DVSA;
+}(this));
+
+/*
+    js module test
+*/
+(function (globals) {
+    'use strict';
+
+    var init,
+        jsTest,
+        JSTest,
+        DVSA = globals.DVSA || {};
+
+    JSTest = function JSTest() {};
+
+    JSTest.prototype.init = function(){
+
+        function test () {
+            console.log('js test');
+        }
+    };
+
+    globals.DVSATest = {
+        jsTest: JSTest
+    };
+
+}(this));
+
+/*jslint browser: true, evil: false, plusplus: true */
+/*global DVSA, $, DVSASelectToggle */
+
+(function (globals) {
+    'use strict';
+    // Add modules
+    // globals.DVSA.Modules.DVSASelectToggle = DVSASelectToggle;
+    globals.DVSA.Modules.DVSATest = DVSATest;
+
+    // Initialise
+    globals.DVSA.init($({}));
+}(this));
