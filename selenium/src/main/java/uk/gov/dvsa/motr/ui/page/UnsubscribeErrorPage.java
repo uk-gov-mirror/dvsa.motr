821fda1af685a139b6ab256b4b68ef1918bb9608
James Wragg <james.wragg@dvsa.gov.uk>
2017-09-25 13:01:01 +0100

    BL-5935: Modified header classes, ensured error templates were consistant

--- selenium/src/main/java/uk/gov/dvsa/motr/ui/page/UnsubscribeErrorPage.java
+++ selenium/src/main/java/uk/gov/dvsa/motr/ui/page/UnsubscribeErrorPage.java
@@ -5,27 +5,12 @@ import org.openqa.selenium.support.FindBy;
 
 import uk.gov.dvsa.motr.navigation.GotoUrl;
 import uk.gov.dvsa.motr.ui.base.Page;
-import uk.gov.dvsa.motr.ui.base.PageIdentityVerificationException;
 
 @GotoUrl("/unsubscribe/{0}")
 public class UnsubscribeErrorPage extends Page {
 
-    @FindBy(className = "heading-xlarge")
-    private WebElement headerTitle;
-
     @FindBy(id = "error-message")
     private WebElement errorMessage;
-
-    @Override
-    protected void selfVerify() {
-
-        if (!getHeaderTitle().contains(getContentHeader()) || !this.driver.getTitle().equals(getPageTitle())) {
-
-            throw new PageIdentityVerificationException("Page identity verification failed: "
-                    + String.format("\n Expected: %s page, \n Found: %s page", getContentHeader(), getHeaderTitle())
-            );
-        }
-    }
 
     @Override
     protected String getContentHeader() {
@@ -37,11 +22,6 @@ public class UnsubscribeErrorPage extends Page {
     protected String getPageTitle() {
 
         return "No MOT reminder found – MOT reminders";
-    }
-
-    public String getHeaderTitle() {
-
-        return headerTitle.getText();
     }
 
     public String getErrorMessageText() {
