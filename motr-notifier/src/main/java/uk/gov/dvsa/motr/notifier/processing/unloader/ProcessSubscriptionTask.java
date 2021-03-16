40ee69f218f222077f1796ff52ac8ed89cd53a8e
Stewart Duffey <stewart.duffey@dvsa.gov.uk>
2021-03-16 17:05:48 +0000

    BL-11344 reinstate exception logging in ProcessSubscriptionTask

--- motr-notifier/src/main/java/uk/gov/dvsa/motr/notifier/processing/unloader/ProcessSubscriptionTask.java
+++ motr-notifier/src/main/java/uk/gov/dvsa/motr/notifier/processing/unloader/ProcessSubscriptionTask.java
@@ -1,11 +1,20 @@
 package uk.gov.dvsa.motr.notifier.processing.unloader;
 
 import uk.gov.dvsa.motr.eventlog.EventLogger;
+import uk.gov.dvsa.motr.notifier.events.HgvPsvDetailsRetrievalFailedEvent;
 import uk.gov.dvsa.motr.notifier.events.NotifyEvent;
+import uk.gov.dvsa.motr.notifier.events.NotifyReminderFailedEvent;
 import uk.gov.dvsa.motr.notifier.events.SubscriptionProcessedEvent;
+import uk.gov.dvsa.motr.notifier.events.SubscriptionProcessingFailedEvent;
 import uk.gov.dvsa.motr.notifier.events.SuccessfulSubscriptionProcessedEvent;
+import uk.gov.dvsa.motr.notifier.events.VehicleDetailsRetrievalFailedEvent;
+import uk.gov.dvsa.motr.notifier.events.VehicleNotFoundEvent;
 import uk.gov.dvsa.motr.notifier.processing.model.SubscriptionQueueItem;
 import uk.gov.dvsa.motr.notifier.processing.service.ProcessSubscriptionService;
+import uk.gov.dvsa.motr.notifier.processing.service.VehicleNotFoundException;
+import uk.gov.dvsa.motr.vehicledetails.HgvPsvDetailsClientException;
+import uk.gov.dvsa.motr.vehicledetails.VehicleDetailsClientException;
+import uk.gov.service.notify.NotificationClientException;
 
 public class ProcessSubscriptionTask {
 
@@ -23,11 +32,42 @@ public class ProcessSubscriptionTask {
 
         Long startedProcessingTime = System.currentTimeMillis();
 
-        processSubscriptionService.processSubscription(subscriptionQueueItemToProcess);
+        try {
+            processSubscriptionService.processSubscription(subscriptionQueueItemToProcess);
 
-        EventLogger.logEvent(new SuccessfulSubscriptionProcessedEvent()
-                .setMessageProcessTimeProcessed(System.currentTimeMillis() - startedProcessingTime)
-                .setMessageBody(subscriptionQueueItemToProcess.toString()));
+            EventLogger.logEvent(new SuccessfulSubscriptionProcessedEvent()
+                    .setMessageProcessTimeProcessed(System.currentTimeMillis() - startedProcessingTime)
+                    .setMessageBody(subscriptionQueueItemToProcess.toString()));
+        } catch (VehicleNotFoundException e) {
+
+            SubscriptionProcessedEvent event = populateEvent(new VehicleNotFoundEvent());
+            EventLogger.logErrorEvent(event, e);
+            throw e;
+
+        } catch (VehicleDetailsClientException e) {
+
+            SubscriptionProcessedEvent event = populateEvent(new VehicleDetailsRetrievalFailedEvent());
+            EventLogger.logErrorEvent(event, e);
+            throw e;
+
+        } catch (HgvPsvDetailsClientException e) {
+
+            SubscriptionProcessedEvent event = populateEvent(new HgvPsvDetailsRetrievalFailedEvent());
+            EventLogger.logErrorEvent(event, e);
+            throw e;
+
+        } catch (NotificationClientException e) {
+
+            NotifyEvent event = populateEvent(new NotifyReminderFailedEvent());
+            EventLogger.logErrorEvent(event, e);
+            throw e;
+
+        } catch (Exception e) {
+            EventLogger.logErrorEvent(new SubscriptionProcessingFailedEvent()
+                    .setMessageBody(subscriptionQueueItemToProcess.toString())
+                    .setMessageProcessTimeProcessed(System.currentTimeMillis() - startedProcessingTime), e);
+            throw e;
+        }
     }
 
     private NotifyEvent populateEvent(NotifyEvent event) {
