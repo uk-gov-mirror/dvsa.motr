9f19cfee208c7ce0ed61723e6a6c684e7118e383
Joshua License <joshua.license@dvsa.gov.uk>
2021-03-17 14:31:02 +0000

    Tweak integration test by polling SQS to determine processing has finished

--- motr-notifier/src/integration-test/java/uk/gov/dvsa/motr/test/integration/message/SubscriptionQueueMessageAbstractTest.java
+++ motr-notifier/src/integration-test/java/uk/gov/dvsa/motr/test/integration/message/SubscriptionQueueMessageAbstractTest.java
@@ -1,16 +1,18 @@
 package uk.gov.dvsa.motr.test.integration.message;
 
-import com.amazonaws.services.lambda.runtime.ClientContext;
-import com.amazonaws.services.lambda.runtime.CognitoIdentity;
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.GetQueueAttributesResult;
+import com.amazonaws.services.sqs.model.QueueAttributeName;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.After;
 import org.junit.Before;
 
+import uk.gov.dvsa.motr.config.Config;
+import uk.gov.dvsa.motr.config.EnvironmentVariableConfig;
+import uk.gov.dvsa.motr.notifier.SystemVariable;
 import uk.gov.dvsa.motr.notifier.component.subscription.persistence.DynamoDbSubscriptionRepository;
 import uk.gov.dvsa.motr.notifier.component.subscription.persistence.SubscriptionDbItem;
 import uk.gov.dvsa.motr.notifier.handler.EventHandler;
@@ -22,11 +24,9 @@ import uk.gov.dvsa.motr.test.integration.dynamodb.fixture.model.SubscriptionTabl
 import uk.gov.dvsa.motr.test.integration.lambda.LoaderHelper;
 import uk.gov.dvsa.motr.test.integration.lambda.LoaderInvocationEvent;
 
-import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 
@@ -65,6 +65,56 @@ public abstract class SubscriptionQueueMessageAbstractTest {
         return jsonMapper.writeValueAsString(loaderInvocationEvent);
     }
 
+    protected void waitForSqsProcessing() throws Exception {
+        int tries = 0;
+
+        while (hasMessagesInQueue()) {
+            if (tries > 5) {
+                System.out.println("Reach maximum attempts of waiting for the SQS to finish.");
+                // Maybe throw an exception here to prevent the rest of the tests from processing.
+                // Something is wrong.
+                break;
+            }
+
+            System.out.println("Messages still in queue. Waiting 5 seconds...");
+            Thread.sleep(5000);
+
+            ++tries;
+        }
+    }
+
+    protected boolean hasMessagesInQueue() {
+        Config config = new EnvironmentVariableConfig();
+
+        AmazonSQS sqsClient = AmazonSQSClientBuilder.defaultClient();
+
+        List<String> attributes = new ArrayList<>();
+        attributes.add(QueueAttributeName.ApproximateNumberOfMessagesDelayed.toString());
+        attributes.add(QueueAttributeName.ApproximateNumberOfMessagesNotVisible.toString());
+        attributes.add(QueueAttributeName.ApproximateNumberOfMessages.toString());
+
+        GetQueueAttributesResult getQueueAttributesResult = sqsClient
+                .getQueueAttributes(config.getValue(SystemVariable.SUBSCRIPTIONS_QUEUE_URL), attributes);
+
+        boolean hasDelayedMessages = !getQueueAttributesResult.getAttributes()
+                .get(QueueAttributeName.ApproximateNumberOfMessagesDelayed.toString()).equals("0");
+
+        boolean hasNotVisibleMessages = !getQueueAttributesResult.getAttributes()
+                .get(QueueAttributeName.ApproximateNumberOfMessagesNotVisible.toString()).equals("0");
+
+        boolean hasMessages = !getQueueAttributesResult.getAttributes()
+                .get(QueueAttributeName.ApproximateNumberOfMessages.toString()).equals("0");
+
+        System.out.println("Delayed messages: " + getQueueAttributesResult.getAttributes()
+                .get(QueueAttributeName.ApproximateNumberOfMessagesDelayed.toString()));
+        System.out.println("Not visible messages: " + getQueueAttributesResult.getAttributes()
+                .get(QueueAttributeName.ApproximateNumberOfMessagesNotVisible.toString()));
+        System.out.println("Total messages: " + getQueueAttributesResult.getAttributes()
+                .get(QueueAttributeName.ApproximateNumberOfMessages.toString()));
+
+        return hasDelayedMessages || hasNotVisibleMessages || hasMessages;
+    }
+
     /**
      * Processes a subscriptionItem. Saves to db, processes, returns updated version (or null if was deleted).
      * @param subscriptionItem The subscription item to save.
@@ -79,34 +129,7 @@ public abstract class SubscriptionQueueMessageAbstractTest {
         String testTime = subscriptionItem.getMotDueDate().minusMonths(1) + "T12:00:00Z";
         loaderHelper.invokeLoader(buildLoaderRequest(testTime));
 
-        // Invoke the notifiers handle event with correct date to read items from the queue.
-        //NotifierReport report = eventHandler.handle(new SQSEvent(), buildContext());
-        SubscriptionQueueItem subscriptionQueueItem = new SubscriptionQueueItem();
-
-        ContactDetail contactDetail = new ContactDetail(subscriptionItem.getEmail(), subscriptionItem.getContactType());
-        LocalDate date = LocalDate.parse(subscriptionItem.getMotDueDate().minusMonths(1).toString());
-
-        subscriptionQueueItem.setMotTestNumber(subscriptionItem.getMotTestNumber());
-        subscriptionQueueItem.setLoadedOnDate(date);
-        subscriptionQueueItem.setContactDetail(contactDetail);
-        subscriptionQueueItem.setDvlaId(subscriptionItem.getDvlaId());
-        subscriptionQueueItem.setId(subscriptionItem.getId());
-        subscriptionQueueItem.setVrm(subscriptionItem.getVrm());
-        subscriptionQueueItem.setVehicleType(subscriptionItem.getVehicleType());
-        subscriptionQueueItem.setMotDueDate(subscriptionItem.getMotDueDate());
-
-        ObjectMapper jsonMapper = new ObjectMapper();
-        SQSEvent.SQSMessage message = new SQSEvent.SQSMessage();
-
-        message.setBody(jsonMapper.writeValueAsString(subscriptionQueueItem));
-        List<SQSEvent.SQSMessage> messageList = Arrays.asList(message);
-
-        SQSEvent event = new SQSEvent();
-        event.setRecords(messageList);
-        eventHandler.handle(event, buildContext());
-
-        // @todo assert response code
-        //assertEquals(1, report.getSuccessfullyProcessed());
+        waitForSqsProcessing();
 
         Optional<SubscriptionDbItem> subscriptionContainer = repo.findById(subscriptionItem.getId());
         if (!subscriptionContainer.isPresent()) {
@@ -127,65 +150,5 @@ public abstract class SubscriptionQueueMessageAbstractTest {
         assertEquals(subscriptionItem.getEmail(), subscriptionDbItem.getEmail());
         assertEquals(subscriptionItem.getMotDueDate(), subscriptionDbItem.getMotDueDate());
         assertEquals(subscriptionItem.getVehicleType(), subscriptionDbItem.getVehicleType());
-    }
-
-    protected Context buildContext() {
-        return new Context() {
-            @Override
-            public String getAwsRequestId() {
-                return UUID.randomUUID().toString();
-            }
-
-            @Override
-            public String getLogGroupName() {
-                return null;
-            }
-
-            @Override
-            public String getLogStreamName() {
-                return null;
-            }
-
-            @Override
-            public String getFunctionName() {
-                return null;
-            }
-
-            @Override
-            public String getFunctionVersion() {
-                return null;
-            }
-
-            @Override
-            public String getInvokedFunctionArn() {
-                return null;
-            }
-
-            @Override
-            public CognitoIdentity getIdentity() {
-                return null;
-            }
-
-            @Override
-            public ClientContext getClientContext() {
-                return null;
-            }
-
-            @Override
-            public int getRemainingTimeInMillis() {
-
-                return 400000;
-            }
-
-            @Override
-            public int getMemoryLimitInMB() {
-                return 0;
-            }
-
-            @Override
-            public LambdaLogger getLogger() {
-                return null;
-            }
-        };
     }
 }
